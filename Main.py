8a939b0a52a6a87a601c2facb05d994ba0e56e3f
Faadil-1 <165633283+Faadil-1@users.noreply.github.com>
2024-05-08 14:53:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import os
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
@@ -11,7 +10,6 @@ st.set_page_config(page_title="Minger Dashboard", page_icon=":rocket:",layout="w
 st.title(" :bulb: DASHBOARD FOR MINGER STORE")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
-os.chdir(r"C:\Users\Faadil\Data Science Individual cw")
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrX4yD6aQ7BJwJphBnAPuUYX4mBrI1n0eF8DF8vO3nzgErzoMQcMWpvabsn3ACOBxF5-Gl839ZC4yY/pub?output=csv", encoding = "ISO-8859-1")
 mba = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRDf5CjG5qpFiF9veB1Afbx3c8qwRf_xHu2WG5ngMNVm51bAXNwZUdMKoAqLlOLiUi7UUvmOTl9BeCl/pub?output=csv", encoding = "ISO-8859-1")
 
